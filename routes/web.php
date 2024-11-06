5ef4912c2e9b8042c593d2a2d155fd914a8fcba0
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2024-11-06 10:57:56 +0700

    create routes

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware('role:pengurus');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified','role:santri|pengurus'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
 
 require __DIR__.'/auth.php';
