b9c3c4fccd1521ded1116ed17a5f2e697bbdd134
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2024-11-09 11:51:18 +0700

    Update permission.php

--- config/permission.php
+++ config/permission.php
@@ -12,6 +12,7 @@ return [
          * The model you want to use as a Permission model needs to implement the
          * `Spatie\Permission\Contracts\Permission` contract.
          */
+        'user' => App\Models\User::class,
 
         'permission' => Spatie\Permission\Models\Permission::class,
 
