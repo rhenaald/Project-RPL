5637677768659c76a5b0242c6273c449f18f8d39
Ikhwan kurniawan <128669107+rhenaald@users.noreply.github.com>
2024-11-11 11:51:36 +0700

    change ot poppins

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,11 +10,12 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-poppins">
         <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
 
